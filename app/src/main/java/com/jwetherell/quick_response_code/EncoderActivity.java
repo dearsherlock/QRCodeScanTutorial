1bba55fffdac55926599a3fff2399753971d392a
proaben <proaben@gmail.com>
2014-11-04 08:51:43 +0800

    可新增產生qrcode文字

--- app/src/main/java/com/jwetherell/quick_response_code/EncoderActivity.java
+++ app/src/main/java/com/jwetherell/quick_response_code/EncoderActivity.java
@@ -15,11 +15,13 @@
 package com.jwetherell.quick_response_code;
 
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -35,22 +37,18 @@ import com.jwetherell.quick_response_code.qrcode.QRCodeEncoder;
  * @author Justin Wetherell (phishman3579@gmail.com)
  */
 public final class EncoderActivity extends Activity {
+    public void GenerateQRCode(View view) {
+       GenBitMapQRCode();
 
-    private static final String TAG = EncoderActivity.class.getSimpleName();
 
-    @Override
-    public void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
-        setContentView(R.layout.encoder);
-
-        // This assumes the view is full screen, which is a good assumption
+    }
+    private void GenBitMapQRCode(){
         WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
         Display display = manager.getDefaultDisplay();
         int width = display.getWidth();
         int height = display.getHeight();
         int smallerDimension = width < height ? width : height;
         smallerDimension = smallerDimension * 7 / 8;
-
         try {
             QRCodeEncoder qrCodeEncoder = null;
             // qrCodeEncoder = new QRCodeEncoder("AT", null, Contents.Type.TEXT,
@@ -72,7 +70,8 @@ public final class EncoderActivity extends Activity {
             // qrCodeEncoder = new QRCodeEncoder("2345", null,
             // Contents.Type.TEXT, BarcodeFormat.PDF_417.toString(),
             // smallerDimension);
-            qrCodeEncoder = new QRCodeEncoder("Hello", null, Contents.Type.TEXT, BarcodeFormat.QR_CODE.toString(), smallerDimension);
+            EditText contents = (EditText) findViewById(R.id.contents_text_viewText);
+            qrCodeEncoder = new QRCodeEncoder(contents.getEditableText().toString(), null, Contents.Type.TEXT, BarcodeFormat.QR_CODE.toString(), smallerDimension);
             // qrCodeEncoder = new QRCodeEncoder("12345678910", null,
             // Contents.Type.TEXT, BarcodeFormat.UPC_A.toString(),
             // smallerDimension);
@@ -81,7 +80,7 @@ public final class EncoderActivity extends Activity {
             ImageView view = (ImageView) findViewById(R.id.image_view);
             view.setImageBitmap(bitmap);
 
-            TextView contents = (TextView) findViewById(R.id.contents_text_view);
+            //EditText contents = (EditText) findViewById(R.id.contents_text_view);
             contents.setText(qrCodeEncoder.getDisplayContents());
             setTitle(getString(R.string.app_name) + " - " + qrCodeEncoder.getTitle());
         } catch (WriterException e) {
@@ -89,5 +88,16 @@ public final class EncoderActivity extends Activity {
         } catch (IllegalArgumentException e) {
             Log.e(TAG, "Could not encode barcode", e);
         }
+
+    }
+
+    private static final String TAG = EncoderActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.encoder);
+
+        GenBitMapQRCode();
     }
 }
--- app/src/main/java/com/jwetherell/quick_response_code/MainActivity.java
+++ app/src/main/java/com/jwetherell/quick_response_code/MainActivity.java
@@ -1,11 +1,12 @@
 package com.jwetherell.quick_response_code;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+import android.view.View;
 public class MainActivity extends Activity {
 
     @Override
@@ -13,6 +14,33 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void generateQRCode(View view) {
+        Intent intent = new Intent(this, EncoderActivity.class);
+
+        // 呼叫「startActivity」，參數為一個建立好的Intent物件
+        // 這行敘述執行以後，如果沒有任何錯誤，就會啟動指定的元件
+        startActivity(intent);
+
+        // 顯示訊息框，指定三個參數
+        // Context：通常指定為「this」
+        // String或int：設定顯示在訊息框裡面的訊息或文字資源
+        // int：設定訊息框停留在畫面的時間
+        //Toast.makeText(this, R.string.app_name, Toast.LENGTH_LONG).show();
+    }
+    public void scanQRCode(View view) {
+        Intent intent = new Intent(this, CaptureActivity.class);
+
+        // 呼叫「startActivity」，參數為一個建立好的Intent物件
+        // 這行敘述執行以後，如果沒有任何錯誤，就會啟動指定的元件
+        startActivity(intent);
+        // 顯示訊息框，指定三個參數
+        // Context：通常指定為「this」
+        // String或int：設定顯示在訊息框裡面的訊息或文字資源
+        // int：設定訊息框停留在畫面的時間
+        //Toast.makeText(this, R.string.app_name, Toast.LENGTH_LONG).show();
+    }
+
+
 
 
     @Override
